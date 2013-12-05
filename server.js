75164d657203ad17d6a4647f62ba01987cab937b
free0005304 <free005304@amchiFRL0005304.local>
2013-12-05 14:04:15 -0600

    remove native nodejs request and add nodejs module request

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ var express = require('express')
     , http = require('http')
     , exphbs  = require('express3-handlebars')
     , pub = __dirname + '/public'
+    , request = require('request')
     , port = process.env.PORT || 3000;
 
 app = express();
@@ -22,46 +23,27 @@ app.set('view engine', 'handlebars');
 
 app.get('/', function (req, res) {
 
-  http.get('http://divvybikes.com/stations/json', function(response) {
-
-    var body, locations, alphabeticalBody,
+  request('http://divvybikes.com/stations/json', function (error, response, body) {
+    var data,
         dataJSON = [];
-    
-    response.on('data', function(chunk) {
-      dataJSON.push(chunk);
+
+    if (!error && response.statusCode == 200) {
+      dataJSON.push(body);
+    }
+
+    try {
+      data = JSON.parse(dataJSON.join(''));
+    } catch(e){
+      console.error(e);
+    }
+
+    var dataFormated = data.stationBeanList;
+
+    res.render('home', {
+      title: 'Chicago Divvy bikes location',
+      result: dataFormated
     });
 
-    response.on('end', function() {
-
-      try {
-        body = JSON.parse(dataJSON.join(''));
-      } catch(e){
-        console.error(e);
-      }
-
-      locations = body.stationBeanList;
-
-      alphabeticalBody = locations.sort(function(location1, location2) {
-        var newLocation1 = location1.stationName.toLowerCase();
-        var newLocation2 = location2.stationName.toLowerCase();
-
-        if ( newLocation1 < newLocation2 ) {
-            return -1;
-        } else if ( newLocation2 > newLocation2 ) {
-            return 1;
-        }
-        return 0;
-      });
-
-      res.render('home', {
-        title: 'Chicago Divvy bikes location',
-        result: alphabeticalBody
-      });
-
-    });
-
-  }).on('error', function() {
-    console.log('Error')
   });
 
 });
